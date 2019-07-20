5a4bfdddc0735abafc0cc3d8697a64a2b2635731
vv <bilaalanwar0@gmail.com>
2019-07-20 12:36:51 +0500

    extrsa

---
+++ LanguageChange/LanguageChange/LanguageChange.h
@@ -0,0 +1,19 @@
+//
+//  LanguageChange.h
+//  LanguageChange
+//
+//  Created by Bilal on 20/07/2019.
+//  Copyright © 2019 Bilal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LanguageChange.
+FOUNDATION_EXPORT double LanguageChangeVersionNumber;
+
+//! Project version string for LanguageChange.
+FOUNDATION_EXPORT const unsigned char LanguageChangeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LanguageChange/PublicHeader.h>
+
+
---
+++ LanguageChange/LanguageChangeTests/abccc.swift
@@ -0,0 +1,14 @@
+//
+//  abccc.swift
+//  LanguageChangeTests
+//
+//  Created by Bilal on 20/07/2019.
+//  Copyright © 2019 Bilal. All rights reserved.
+//
+
+import Foundation
+public class ABC {
+    func add() {
+        print("error")
+    }
+}
